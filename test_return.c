40505f0934b6cf030baa9a93549d5476997bd173
caozilong <caozilong@allwinnertech.com>
2018-06-21 16:31:23 +0800

    update

--- test_return.c
+++ test_return.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
 extern signed short test_return_sshort() {
     return 102;
 }
+
+int testFunc(int m, int n) {
+  printf("params: %d %d \n", n, m);
+  return n+m;
+}
+
